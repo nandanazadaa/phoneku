27f14bdaacba24e6d8b8702433a36c1408a05e99
ican017 <ikhsankuliah017@gmail.com>
2025-04-16 11:25:28 +0700

    Tim Fiks

--- resources/views/Home/tim.blade.php
+++ resources/views/Home/tim.blade.php
@@ -22,8 +22,8 @@
                         <div class="w-full h-48 rounded-[212px] overflow-hidden mb-4">
                           <img src="img/yusuf.jpg" alt="Muhammad Yusuf Nurwahid" class="w-full h-full object-cover">
                         </div>
-                        <p class="text-sm text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae molestias voluptatum deserunt consequuntur soluta, nisi odio facere eligendi iste assumenda unde dolores numquam laborum perferendis.                        </p>
+                        <p class="text-sm text-gray-600"><i class="fa-solid fa-quote-left"></i>
+                        Kode bukan sekadar instruksi untuk mesin,tapi juga catatan diam dari pikiran manusia yang ingin menciptakan solusi.Setiap kurung yang dibuka harus ditutup.Setiap bug yang muncul adalah tantangan untuk ditaklukkan, bukan musuh yang harus ditakuti. <i class="fa-solid fa-quote-right"></i>                        </p>
                         </p>
                         <div class="flex justify-center gap-4 mt-4 text-gray-600 text-xl">
                           <a href="https://github.com/yusufwahid"><i class="fab fa-github"></i></a>
@@ -32,7 +32,55 @@
                       </div>
                     <!-- Kartu Profil 1 (Naik) -->
                     <div class="relative bg-white rounded-[212px] w-48 pt-10 text-center py-12 px-3 shadow-lg lg:mt-0 lg:mb-40 flex flex-col justify-between min-h-[460px]">
-                        <h2 class="text-xl font-bold">Akbar Rizqy Effendi</h2>
+                    <h2 class="text-xl font-bold">Nandana Zada Abiproya</h2>
+                        <p class="text-sm text-gray-500 mb-4">Frontend Developer</p>
+                        <div class="w-full h-48 rounded-[212px] overflow-hidden mb-4">
+                          <img src="img/zada.jpg" alt="Nandana Zada Abiproya" class="w-full h-full object-cover">
+                        </div>
+                        <p class="text-sm text-gray-600"><i class="fa-solid fa-quote-left"></i>
+                            Petir bukan sembarang petir.Ada yang mau rumahhnya disambar petir? <i class="fa-solid fa-quote-right"></i>                        </p>
+                        </p>
+                        <div class="flex justify-center gap-4 mt-4 text-gray-600 text-xl">
+                        <a href="https://github.com/nandanazadaa"><i class="fab fa-github"></i></a>
+                        <a href="https://www.instagram.com/zada.abiproya/?hl=en"><i class="fab fa-instagram"></i></a>
+                        </div>
+                      </div>
+              
+                    <!-- Kartu Profil 2 (Turun) -->
+                    <div class="relative bg-white rounded-[212px] w-48 pt-10 text-center py-12 px-3 shadow-lg lg:mt-40 flex flex-col justify-between min-h-[460px]">
+                        <h2 class="text-xl font-bold">Muhammad Zayga Ernesto</h2>
+                        <p class="text-sm text-gray-500 mb-4">Frontend Developer</p>
+                        <div class="w-full h-48 rounded-[212px] overflow-hidden mb-4">
+                          <img src="img/zayga.jpg" alt="Muhammad Zayga Ernesto" class="w-full h-full object-cover">
+                        </div>
+                        <p class="text-sm text-gray-600"><i class="fa-solid fa-quote-left"></i>
+                        Belajarku bukan dari buku besar, tapi dari error kecil yang muncul tiap malam. Aku anak hasil eksperimen: antara niat, koneksi Wi-Fi, dan tutorial berdurasi 2x speed. <i class="fa-solid fa-quote-right"></i>                  </p>
+                        </p>
+                        <div class="flex justify-center gap-4 mt-4 text-gray-600 text-xl">
+                        <a href="https://github.com/ZaygaErnesto"><i class="fab fa-github"></i></a>
+                        <a href="https://www.instagram.com/zaygaernesto/?hl=en"><i class="fab fa-instagram"></i></a>
+                        </div>
+                      </div>
+              
+                    <!-- Kartu Profil 3 (Naik) -->
+                    
+                    <div class="relative bg-white rounded-[212px] w-48 pt-10 text-center py-12 px-3 shadow-lg lg:mt-0 lg:mb-40 flex flex-col justify-between min-h-[460px]">
+                    <h2 class="text-xl font-bold">Ikhsanudin</h2>
+                        <p class="text-sm text-gray-500 mb-4">Frontend Developer</p>
+                        <div class="w-full h-48 rounded-[212px] overflow-hidden mb-4">
+                          <img src="img/ikhsan.jpg" alt="Ikhsanudin" class="w-full h-full object-cover">
+                        </div>
+                        <p class="text-sm text-gray-600"><i class="fa-solid fa-quote-left"></i>
+                        Jadilah Orang Pengembang sistem,Kalau tidak mau tergerus Oleh Sistem. <i class="fa-solid fa-quote-right"></i>                  </p>
+                        </p>
+                        <div class="flex justify-center gap-4 mt-4 text-gray-600 text-xl">
+                        <a href="https://github.com/ikhsanudin017"><i class="fab fa-github"></i></a>
+                        <a href="https://www.instagram.com/i_can017/?hl=en"><i class="fab fa-instagram"></i></a>
+                        </div>
+                      </div>
+                      
+                      <div class="relative bg-white rounded-[212px] w-48 pt-10 text-center py-12 px-3 shadow-lg lg:mt-40 flex flex-col justify-between min-h-[460px]">
+                      <h2 class="text-xl font-bold">Akbar Rizqy Effendi</h2>
                         <p class="text-sm text-gray-500 mb-4">Frontend Developer</p>
                         <div class="w-full h-48 rounded-[212px] overflow-hidden mb-4">
                           <img src="img/akbar.jpg" alt="Akbar Rizqy Effendi" class="w-full h-full object-cover">
@@ -44,53 +92,6 @@
                         <a href="https://www.instagram.com/rizqyme/?hl=en"><i class="fab fa-instagram"></i></a>
                         </div>
                       </div>
-              
-                    <!-- Kartu Profil 2 (Turun) -->
-                    <div class="relative bg-white rounded-[212px] w-48 pt-10 text-center py-12 px-3 shadow-lg lg:mt-40 flex flex-col justify-between min-h-[460px]">
-                        <h2 class="text-xl font-bold">Muhammad Zayga Ernesto</h2>
-                        <p class="text-sm text-gray-500 mb-4">Frontend Developer</p>
-                        <div class="w-full h-48 rounded-[212px] overflow-hidden mb-4">
-                          <img src="img/zayga.jpg" alt="Muhammad Zayga Ernesto" class="w-full h-full object-cover">
-                        </div>
-                        <p class="text-sm text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae molestias voluptatum deserunt consequuntur soluta, nisi odio facere eligendi iste assumenda unde dolores numquam laborum perferendis.                        </p>
-                        </p>
-                        <div class="flex justify-center gap-4 mt-4 text-gray-600 text-xl">
-                        <a href="https://github.com/ZaygaErnesto"><i class="fab fa-github"></i></a>
-                        <a href="https://www.instagram.com/zaygaernesto/?hl=en"><i class="fab fa-instagram"></i></a>
-                        </div>
-                      </div>
-              
-                    <!-- Kartu Profil 3 (Naik) -->
-                    <div class="relative bg-white rounded-[212px] w-48 pt-10 text-center py-12 px-3 shadow-lg lg:mt-0 lg:mb-40">
-                        <h2 class="text-xl font-bold">Nandana Zada Abiproya</h2>
-                        <p class="text-sm text-gray-500 mb-4">Frontend Developer</p>
-                        <div class="w-full h-48 rounded-[212px] overflow-hidden mb-4">
-                          <img src="img/zada.jpg" alt="Nandana Zada Abiproya" class="w-full h-full object-cover">
-                        </div>
-                        <p class="text-sm text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae molestias voluptatum deserunt consequuntur soluta, nisi odio facere eligendi iste assumenda unde dolores numquam laborum perferendis.                        </p>
-                        </p>
-                        <div class="flex justify-center gap-4 mt-4 text-gray-600 text-xl">
-                        <a href="https://github.com/nandanazadaa"><i class="fab fa-github"></i></a>
-                        <a href="https://www.instagram.com/zada.abiproya/?hl=en"><i class="fab fa-instagram"></i></a>
-                        </div>
-                      </div>
-                      <div class="relative bg-white rounded-[212px] w-48 pt-10 text-center py-12 px-3 shadow-lg lg:mt-40">
-                        <h2 class="text-xl font-bold">Ikhsanudin</h2>
-                        <p class="text-sm text-gray-500 mb-4">Frontend Developer</p>
-                        <div class="w-full h-48 rounded-[212px] overflow-hidden mb-4">
-                          <img src="img/ikhsan.jpg" alt="Ikhsanudin" class="w-full h-full object-cover">
-                        </div>
-                        <p class="text-sm text-gray-600"><i class="fa-solid fa-quote-left"></i>
-                        Jadilah Orang Pengembang sistem,Kalau tidak mau tergerus Oleh Sistem <i class="fa-solid fa-quote-right"></i></p>
-                        </p>
-                        <div class="flex justify-center gap-4 mt-4 text-gray-600 text-xl">
-                        <a href="https://github.com/ikhsanudin017"><i class="fab fa-github"></i></a>
-                        <a href="https://www.instagram.com/i_can017/?hl=en"><i class="fab fa-instagram"></i></a>
-                        </div>
-                      </div>
-                      
 
                   </div>
                 </div>
