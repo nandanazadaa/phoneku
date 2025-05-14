38db0d57c8935f4bf954cbdf1e79e900f610668c
nandanazadaa <ezzadut@gmail.com>
2025-05-14 11:28:58 +0700

    user chat css fix

--- resources/views/Home/user_chat.blade.php
+++ resources/views/Home/user_chat.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Customer Support Chat</title>
-
     <!-- CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
