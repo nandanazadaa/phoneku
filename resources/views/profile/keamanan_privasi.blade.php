67d6de66c08f55a0e58eba2e357a7d5036f33a98
YusufWahid <yusufwahid2005@gmail.com>
2025-06-21 15:55:56 +0700

    penyamaan tampilan di profile keamanan & privasi

--- resources/views/profile/keamanan_privasi.blade.php
+++ resources/views/profile/keamanan_privasi.blade.php
@@ -4,32 +4,41 @@
 
 @section('content')
 <div class="relative">
-    <div class="bg-blue-500 min-h-[400px] md:min-h-[500px] relative"></div>
+    <div class="bg-blue-500 min-h-[400px] sm:min-h-[400px] md:min-h-[500px] lg:min-h-[400px] xl:min-h-[350px] relative">
+        <!-- Kosongkan konten header jika tidak ada teks -->
+    </div>
 
-    <div class="absolute bottom-[-24px] left-0 w-full overflow-hidden" style="line-height: 0;">
+    <!-- Wave SVG -->
+    <div class="absolute bottom-[-24px] left-0 w-full overflow-hidden wave-container" style="line-height: 0;">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="w-full">
             <path fill="#f9fafb" fill-opacity="1"
-                d="M0,224L48,213.3C96,203,192,181,288,181.3C384,181,480,203,576,224C672,245,768,267,864,266.7C960,267,1056,245,1152,224C1248,203,1344,181,1392,170.7L1440,160L1440,320L0,320Z">
+                d="M0,224L48,213.3C96,203,192,181,288,181.3C384,181,480,203,576,224C672,245,768,267,864,266.7C960,267,1056,245,1152,224C1248,203,1344,181,1392,170.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
             </path>
         </svg>
     </div>
 
+    <!-- Banner Image -->
     <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5 max-w-3xl z-0 pointer-events-none">
-        <img src="{{ asset('img/banner4.png') }}" alt="Smartphones"
-             class="object-contain w-full h-auto max-h-[350px]">
+        <div class="relative w-full h-full overflow-hidden">
+            <img src="{{ asset('img/banner4.png') }}" alt="Smartphones"
+                class="object-contain w-full h-auto max-h-[180px] sm:max-h-[250px] md:max-h-[300px] lg:max-h-[350px] xl:max-h-[400px]">
+        </div>
     </div>
 </div>
 
+<!-- Main Content - User Profile -->
 <div class="container mx-auto px-4 py-8 -mt-48 relative z-10">
-    <div class="flex flex-wrap">
-        <!-- Sidebar -->
-        <div class="w-full md:w-1/4 mb-6 md:mb-0 md:pr-4">
-            <div class="bg-white rounded-xl p-4 shadow-md mb-6 text-center">
-                <div class="w-32 h-32 rounded-full overflow-hidden mx-auto border-2 border-gray-200 shadow-sm mb-4">
-                    <img src="{{ $user->profile && $user->profile->profile_picture ? asset('storage/' . $user->profile->profile_picture) : asset('img/profile.png') }}"
-                         alt="User Profile" class="w-full h-full object-cover">
+    <div class="flex flex-wrap lg:flex-nowrap">
+        <!-- Left Sidebar -->
+        <div class="w-full md:w-1/3 lg:w-1/4 mb-6 md:mb-0 md:pr-4">
+            <div class="bg-white rounded-xl p-4 shadow-md mb-6">
+                <div class="flex flex-col items-center mb-4">
+                    <div class="w-32 h-32 rounded-full overflow-hidden mb-4 border-2 border-gray-200 shadow-sm">
+                        <img src="{{ $user->profile && $user->profile->profile_picture ? asset('storage/' . $user->profile->profile_picture) : asset('img/profile.png') }}"
+                            alt="User Profile" class="w-full h-full object-cover">
+                    </div>
+                    <h2 class="text-xl font-bold mb-1 text-gray-800">{{ $user->name }}</h2>
                 </div>
-                <h2 class="text-xl font-bold text-gray-800">{{ $user->name }}</h2>
             </div>
 
             <div class="bg-white rounded-xl p-4 shadow-md space-y-2">
